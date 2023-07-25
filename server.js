0ef6347d1332b45f28c64e700d28f3ba29c65116
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-07-24 22:27:41 -0700

    Add: mongoose (DB settings)

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-// initiate the backend server app 
+// initiate the backend server app
 const app = express();
-// allow app to use json as content/application type 
+// allow app to use json as content/application type
 app.use(express.json());
 // Enable Express application allows requests from any domain
 app.use(cors());
 
+// get the db address from cmd mongosh
+mongoose
+    .connect("mongodb://127.0.0.1:27017/mern-todo", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to DB"))
+    .catch(console.error);
+
+app.listen(3001, () => console.log("Server started on port 3001"));
